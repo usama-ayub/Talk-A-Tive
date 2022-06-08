f488509f26b03367a934f0063ee316e865c1d851
Usamaayub <usamaayub2012@gmail.com>
2022-06-09 00:49:27 +0500

    frontend initial step add and connect to database

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,13 @@
 const express = require('express');
 const dotenv = require('dotenv');
-const app = express();
+const colors = require("colors");
+
+const connectDB = require('./config/db');
+
 dotenv.config();
 
+connectDB();
+const app = express();
+
 const port = process.env.PORT || 5000;
-app.listen(5000,console.log('Server Start on port 5000'))
+app.listen(5000,console.log(`Server running on PORT ${port}...`.yellow.bold))
---
+++ frontend/src/pages/Chat.js
@@ -0,0 +1,13 @@
+import { useEffect } from "react";
+import { useHistory } from "react-router";
+
+function ChatPage() {
+  
+    return (
+     <div>
+         Chat Page
+     </div>
+    );
+  }
+  
+  export default ChatPage;
